63e4aa82365fe37656573ed26d3668e103b2864a
celicaaa <la-astrahankina@yandex.ru>
2024-11-12 17:26:26 +0300

    Update Maze_from_Astrahankina.h

--- Code_Here/Maze_from_Astrahankina.h
+++ Code_Here/Maze_from_Astrahankina.h
@@ -1,1 +1,35 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+enum CellType {
+    Wall = 1,
+    Path = 0
+};
+
+class Maze1 {
+public:
+    Maze1(int size);
+    void generateMazePrim();
+    void printMaze();
+
+    // Accessor for the maze grid
+    const std::vector<std::vector<int>>& getMaze() const;
+
+    // Pathfinding functions
+    bool findShortestPathDijkstra(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
+    bool findShortestPathDFS(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
+    void printMazeWithPath(const std::vector<std::pair<int, int>>& path);
+
+private:
+    int size;
+    std::vector<std::vector<int>> maze;
+
+    // Utility functions
+    bool inBounds(int x, int y);
+    std::vector<std::pair<int, int>> getNeighbours(int x, int y);
+
+    // For DFS
+    bool dfs(int x, int y, int endX, int endY, std::vector<std::pair<int, int>>& path, std::vector<std::vector<bool>>& visited);
+};
 
