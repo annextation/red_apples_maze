7a88920a34d0ed4bb3319151ca2ba795a68e75ab
celicaaa <la-astrahankina@yandex.ru>
2024-11-18 09:49:19 +0300

    Change Maze_from_Astrahankina.h

--- Code_Here/Maze_from_Astrahankina.h
+++ Code_Here/Maze_from_Astrahankina.h
@@ -1,35 +1,27 @@
 #pragma once
 #include <vector>
 #include <utility>
+#include <iostream>
 
 enum CellType {
     Wall = 1,
     Path = 0
 };
 
-class Maze1 {
-public:
-    Maze1(int size);
-    void generateMazePrim();
-    void printMaze();
 
-    // Accessor for the maze grid
-    const std::vector<std::vector<int>>& getMaze() const;
-
-    // Pathfinding functions
-    bool findShortestPathDijkstra(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
-    bool findShortestPathDFS(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
-    void printMazeWithPath(const std::vector<std::pair<int, int>>& path);
-
-private:
+struct Maze1 {
     int size;
-    std::vector<std::vector<int>> maze;
-
-    // Utility functions
-    bool inBounds(int x, int y);
-    std::vector<std::pair<int, int>> getNeighbours(int x, int y);
-
-    // For DFS
-    bool dfs(int x, int y, int endX, int endY, std::vector<std::pair<int, int>>& path, std::vector<std::vector<bool>>& visited);
+    std::vector<std::vector<int>> grid;
 };
 
+
+Maze1 createMaze(int size);
+void generateMazePrim(Maze1& maze);
+void printMaze(const Maze1& maze);
+
+bool inBounds(const Maze1& maze, int x, int y);
+std::vector<std::pair<int, int>> getNeighbours(const Maze1& maze, int x, int y);
+
+bool findShortestPathDijkstra(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
+bool findShortestPathDFS(int startX, int startY, int endX, int endY, std::vector<std::pair<int, int>>& path);
+void printMazeWithPath(const std::vector<std::pair<int, int>>& path);
