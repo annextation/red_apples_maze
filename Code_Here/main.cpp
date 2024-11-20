d8957d6b705ea4b82e9e2b27de2c172697f9ee4d
annextation <150847325+annextation@users.noreply.github.com>
2024-11-20 14:28:41 +0300

    Update main.cpp

--- Code_Here/main.cpp
+++ Code_Here/main.cpp
@@ -1,9 +1,9 @@
 #include "Maze_from_Vivcharic.h"
 #include "Maze_from_Astrahankina.h"
 #include "start.h"
-#include <format>
 
 using namespace std;
+
 void print_menu() {
     cout << "--------------------" << endl;
     cout << "Methods: " << endl;
@@ -11,21 +11,6 @@ void print_menu() {
     cout << "2. Prima" << endl;
     cout << "0. Exit" << endl;
     cout << "> ";
-}
-
-template <typename Type>
-Type GetCorrectNumber(Type min, Type max) {
-    Type x;
-    while ((std::cin >> x).fail()
-        || std::cin.peek() != '\n'
-        || x < min || x > max)
-    {
-        std::cin.clear();
-        std::cin.ignore(1000, '\n');
-        std::cout << "Enter a number of the type " << typeid(Type).name() << " in the range from " << min << " to " << max << ": ";
-    }
-    //std::cerr << x << std::endl;
-    return x;
 }
 
 int main() {
@@ -39,10 +24,11 @@ int main() {
             start_search_labirint_1();
             break;
         case 2:
+
             start_search_labirint_2();
         }
     } while (input != 0);
-        
+
     return 0;
 }
 
