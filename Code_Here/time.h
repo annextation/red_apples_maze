e71aa6d9660b6b9c8228d44d7c6318ee0bf7d506
annextation <150847325+annextation@users.noreply.github.com>
2024-11-20 14:30:05 +0300

    Update time.h

--- Code_Here/time.h
+++ Code_Here/time.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "Maze_from_Vivcharic.h"
+#include "Maze_from_Astrahankina.h"
+#include "time.h"
+#include <chrono>
 
-double time_generate_maze_from_Vivcharic(Maze& maze);
-
-double time_generate_maze_from_Astrahankina(Maze1& maze);
+template <typename MazeType, typename GenerateMazeFunc>
+double time_generation(MazeType& maze, GenerateMazeFunc generateFunc) {
+    auto start = std::chrono::high_resolution_clock::now();
+    generateFunc(maze);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    return duration.count();
+}
