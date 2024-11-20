11a1134bbf6f0c646983c29baa3125985f2beb4d
annextation <150847325+annextation@users.noreply.github.com>
2024-11-20 14:29:50 +0300

    Update start.h

--- Code_Here/start.h
+++ Code_Here/start.h
@@ -1,4 +1,22 @@
 #pragma once
+#include <iostream>
+#include <format>
+
+
+template <typename Type>
+Type GetCorrectNumber(Type min, Type max) {
+    Type x;
+    while ((std::cin >> x).fail()
+        || std::cin.peek() != '\n'
+        || x < min || x > max)
+    {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cout << "Enter a number of the type " << typeid(Type).name() << " in the range from " << min << " to " << max << ": ";
+    }
+    //std::cerr << x << std::endl;
+    return x;
+}
 
 void start_search_labirint_1();
 
