99c1b42d4773a91d5d88a5f56081a19e47055400
annextation <150847325+annextation@users.noreply.github.com>
2024-11-20 14:29:35 +0300

    Update start.cpp

--- Code_Here/start.cpp
+++ Code_Here/start.cpp
@@ -1,108 +1,88 @@
 #include "start.h"
 #include "Maze_from_Vivcharic.h"
 #include "Maze_from_Astrahankina.h"
-#include "verification.h"
 #include "time.h"
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void start_search_labirint_1(){
-    int size;
-    cout << "Size: ";
-    while (1) {
-        cin >> size;
-        if (size >= 73 || size < 3 || size % 2 == 0) {
-            cout << "Incorect input, try again" << endl << "> ";
-        }
-        else {
-            break;
-        }
-    }
-    
 
-    Maze maze(size);
-    double time_maze1 = time_generate_maze_from_Vivcharic(maze);
-    maze.display();
-    cout << "The maze was created for " << time_maze1 * 1000 << " ms." << endl;
-    verifyMazeSaving;
-    // Определяем начальную и конечную точки
-    int startX = 0, startY = 1;
-    int endX = size - 1, endY = size - 2;
-    
-    // Поиск кратчайшего пути
+
+void start_search_labirint_1() {
+    cout << "Input size: ";
+    int size = GetCorrectNumber<int>(5, 73);
+
+    Maze maze = { size };
+    cout << "The maze was created for " << time_generation(maze, generateMaze) * 1000 << " ms." << endl;
+    displayMaze(maze);
+
+    vector<pair<int, int>> pathDijkstra;
+    auto start = std::chrono::high_resolution_clock::now();
+    bool foundDijkstra = findShortestPathDijkstra(maze, 0, 1, maze.size - 1, maze.size - 2, pathDijkstra);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    if (foundDijkstra) {
+        cout << "Dijkstra: " << endl << duration.count() * 1000 << " ms." << endl;
+        cout << "Steps Dijkstra: " << calculatePathDijkstraLength(pathDijkstra) << endl;
+    }
+    else {
+        cout << "No path found." << endl;
+    }
+
     vector<pair<int, int>> path;
-    if (maze.findShortestPathDFS1(startX, startY, endX, endY, path)) {
-        //        for (const auto& p : path) {
-        //            cout << "(" << p.first << ", " << p.second << ") ";
-        //        }
-
-                // Вычисление длины пути
-        int pathLength = maze.calculatePathLength1(path);
-        cout << "Steps: " << pathLength << endl;
+    auto start1 = std::chrono::high_resolution_clock::now();
+    bool foundDFS = findShortestPathDFS(maze, 0, 1, maze.size - 1, maze.size - 2, path);
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration1 = end1 - start1;
+    if (foundDFS) {
+        cout << "DFS: " << endl << duration1.count() * 1000 << " ms." << endl;
+        cout << "Steps DFS: " << calculatePathLength(path) << endl;
     }
     else {
-        cout << "Error.";
+        cout << "No path found." << endl;
     }
-    cout << endl;
-
-    // Поиск кратчайшего пути методом Дейкстры
-    if (maze.findShortestPathDijkstra(startX, startY, endX, endY, path)) {
-        //      for (const auto& p : path) {
-        //             cout << "(" << p.first << ", " << p.second << ") ";
-        //        }
-
-                // Вычисление длины пути
-        int pathLength = maze.calculatePathDijkstraLength1(path);
-        cout << "Steps: " << pathLength << endl;
-    }
-    else {
-        cout << "Error" << endl;
-    }
-    cout << endl;
-
 }
 
-void start_search_labirint_2(){
-        int size;
-        cout << "Size: ";
-        while (1) {
-            cin >> size;
-            if (size >= 73 || size < 3 || size % 2 == 0) {
-                cout << "Incorect input, try again" << endl << "> ";
-            }
-            else {
-                break;
-            }
-        }
+void start_search_labirint_2() {
+    cout << "Input size: ";
+    int size = GetCorrectNumber<int>(5, 73);
 
+    Maze1 maze = createMaze(size);
+    cout << "The maze was created for " << time_generation(maze, generateMazePrim) * 1000 << " ms." << endl;
+    printMaze(maze);
 
-        Maze1 maze(size);
-        double time_maze2 = time_generate_maze_from_Astrahankina(maze);
-        maze.printMaze();
-        cout << "The maze was created for " << time_maze2 * 1000 << " ms." << endl;
+    int startX = 1, startY = 1;
+    int endX = size - 2, endY = size - 2;
 
-        int startX = 1, startY = 1;
-        int endX = size - 2, endY = size - 2;
+    vector<pair<int, int>> pathDijkstra;
+    auto start = std::chrono::high_resolution_clock::now();
+    bool foundDijkstra = findShortestPathDijkstra(maze, startX, startY, endX, endY, pathDijkstra);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    if (foundDijkstra) {
+        
+        cout << "Dijkstra: " << endl << duration.count() * 1000 << " ms." << endl;
+        cout << "Steps Dijkstra: " << calculatePathLength(pathDijkstra) << endl;
 
-        std::vector<std::pair<int, int>> pathDijkstra;
-        if (maze.findShortestPathDijkstra(startX, startY, endX, endY, pathDijkstra)) {
-            //std::cout << "\nPath found using Dijkstra's Algorithm:\n";
-            //maze.printMazeWithPath(pathDijkstra);
-        }
-        else {
-            std::cout << "No path found using Dijkstra's Algorithm.\n";
-        }
+    }
+    else {
+        std::cout << "No path found." << endl;
+    }
 
-        std::vector<std::pair<int, int>> pathDFS;
-        if (maze.findShortestPathDFS(startX, startY, endX, endY, pathDFS)) {
-            //std::cout << "\nPath found using Depth-First Search:\n";
-            //maze.printMazeWithPath(pathDFS);
-        }
-        else {
-            std::cout << "No path found using Depth-First Search.\n";
-        }
-
+    
+    std::vector<std::pair<int, int>> pathDFS;
+    auto start1 = std::chrono::high_resolution_clock::now();
+    bool foundDFS = findShortestPathDFS(maze, startX, startY, endX, endY, pathDFS);
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration1 = end1 - start1;
+    if (foundDFS) {
+        
+        cout << "DFS: " << endl << duration1.count() * 1000 << " ms." << endl;
+        cout << "Steps DFS: " << calculatePathLength(pathDFS) << endl;
+    }
+    else {
+        std::cout << "No path found ." << endl;
+    }
 
 }
